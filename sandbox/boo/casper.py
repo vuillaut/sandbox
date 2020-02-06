157f75bf730be795f266cd35906b69e28a3be509
Thomas Vuillaume <thomas.vuillaume@gmail.com>
2020-02-06 16:15:53 +0100

    Update casper.py

--- sandbox/boo/casper.py
+++ sandbox/boo/casper.py
@@ -4,16 +4,6 @@ import os
 def haunt(place='house'):
     print(f'The {place} now is haunted')
 
-
-def unblack_ghost():
-
-    a = 13
-    b = 3
-    c = a + b
-    print( "ho no, this is .................line.............................. is so very long ")
-
-
-
 def pr_unblack_ghost():
     """
     This is submitted through PR to test black action
@@ -21,6 +11,7 @@ def pr_unblack_ghost():
     a = 13
     b = 3
     c = a + b
+    print(c)
     print( "ho no, this is .................line.............................. is so very long ")
 
 
