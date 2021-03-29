f2a3dbfe5dbe9d1162c6385c1b9205bb10323926
Thomas Vuillaume <thomas.vuillaume@gmail.com>
2021-03-29 18:01:39 +0200

    Update casper.py

--- sandbox/boo/casper.py
+++ sandbox/boo/casper.py
@@ -14,7 +14,19 @@ def pr_unblack_ghost():
     print(c)
     print( "ho no, this is .................line.............................. is so very long ")
 
-
+def another_unblack_function(a=0, b=1):
+    """
+        even bad docstring?
+    """
+    if (a == 0 or 
+            b == 0 ):
+        c = 1
+    else:
+        c = 0
+    print(c)
+    return c
+        
+    
 def testing_flake8():
     a = 4
     print(a)
